ec6b58c681b3992c84778d4c57ffaec3f9d0fc09
Cynthia Prevatte <cynthiaprevatte@gmail.com>
2017-05-05 16:34:05 -0700

    problem 4 solved

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -94,45 +94,31 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
 
 
 /* Problem 4
-Write a function called sumArray() that takes in an array of 
-numbers as its single argument and then returns an array where
-the first element is the sum of the numbers in the array, and 
-the second element is a string that EXACTLY follows this example 
-and uses the values that were input into the function:
+Write a function called sumArray() that takes an array of numbers as its single argument and returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this 
-function. To do addition, use your sum() function that you've already
-created. You're going to have to be resourceful to figure out how to do this.
-
-Test this function by hand in the console to get it working, and when
-you think it is finished, uncomment the call for the testSumArray() 
-function and see if the test passes.
-
-Once you get the test passing, do an a-c-p cycle and synchronize the 
-code between GitHub and your laptop. Don't forget to create a 
-new branch for your work on the next question!
-*/
+IMPORTANT use your sum() function that you've already created. */
 
 // Write your code here
+
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
 
+    var sumOf = sum (testArray[0], (sum (testArray[1], testArray[2] ) )[0] );
+    console.log (sumOf[0]);
+    var str = (testArray[0] + "," + testArray[1] + "," + testArray[2] + " " + "was passed in as an array of numbers, and " + sumOf[0] + " is their sum.");
+    console.log( str);
+    return [sumOf, str];
 }
 
-// Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+// test for sumArray(); uncomment it to run it
+testSumArray(testArray);
 
-
-/////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers
-as its argument and returns an array whose first element 
-is the product of those numbers, and the second element is 
-a string that EXACTLY follows this example and uses the values
-that were input into the function:
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is 
+a string that EXACTLY follows this:
 
 "The numbers 2,3,4 have a product of 24."
 
